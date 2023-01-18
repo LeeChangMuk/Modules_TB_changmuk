3b747800471cc21fcb1514c6ad2c2c57629584fb
20211313_조성운 <87853858+JoNebula@users.noreply.github.com>
2023-01-19 00:29:43 +0900

    Update TripBuilder.py

--- TripBuilder.py
+++ TripBuilder.py
@@ -19,7 +19,9 @@ class NaverCrawler:
         self.API_keys = [{"client_id": "0mK4JnoFJM1CPYWNlG80", "client_secret": "UMINhUjvKQ"},
                          {"client_id": "HSGXhbVLnjvb31S9N_cB", "client_secret": "4r9gnASzKU"},
                          {"client_id": "9DUnlYmUPuQJlQht6UTE", "client_secret": "G3mYUv08Vh"},
-                         {"client_id": "Ya1J27IktsR8oawUscJa", "client_secret": "Ax9N8UnIYG"}]
+                         {"client_id": "Ya1J27IktsR8oawUscJa", "client_secret": "Ax9N8UnIYG"},
+                         {"client_id": "q5Vw2F_nDa8imGweLrUN", "client_secret": "bN1fQ6iG4e"}]
+
         self.HEADERS = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"}
 
